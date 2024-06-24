b38ccb9997995f89dcef61b54ae94d01fb6e5701
jonmicallef <jonjonmicallef@gmail.com>
2024-06-24 12:20:45 +0200

    adding youths form

--- js/js.js
+++ js/js.js
@@ -1,8 +1,13 @@
-// Wait for the document to fully load
 document.addEventListener("DOMContentLoaded", function() {
   // Set a timeout to show the modal after 5 seconds
   setTimeout(function() {
     // Show the modal
     $('#rugbyCrashCourseModal').modal('show');
   }, 5000); // 5000ms = 5 seconds
+
+  // Handle the sign-up button click
+  document.getElementById('signUpButton').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default button action
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSeSJ2uCmMwiFD7MooEiC3pc4ij-Y3XlXnTiT5eKHdOOhzPm6g/viewform', '_blank'); // Open the link in a new tab
+  });
 });
